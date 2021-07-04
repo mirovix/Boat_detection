0f42b2a73c2dc3dc7b3964d83cac532d83aed85e
Miro <miroljub.mihailovic@studenti.unipd.it>
2021-07-04 02:53:09 +0200

    Loading a NN and start testing some images

--- source/detecting_boats.cpp
+++ source/detecting_boats.cpp
@@ -2,23 +2,60 @@
 
 int main(int argc, char** argv)
 {
+	const string pb_model = "C:/data/model/model.pb";
+	const string pbtxt_model = "C:/data/model/model.pbtxt";
+	const string img_path = "C:/data/train/20130304_054304_04214.jpg";
+	
+	Mat image = imread(img_path);
+	Net model = readNetFromTensorflow(pb_model, pbtxt_model);
+
+	setUseOptimized(true);
+	setNumThreads(4);
 
 	/*
-	string line;
-	FILE* fmyfile = fopen("C:\data\label.json", "r");
-	while (getline(myfile, line)) {
-
-		cout << line << endl << "-";
-		printf("%s", line);
-	}
-	myfile.close();
-	system("PAUSE");
-	return 0;
+	Mat blob2 = blobFromImage(image, 1.0, Size(299, 299), Scalar(127.5, 127.5, 127.5), true, false);
+	model.setInput(blob2);
+	Mat output2 = model.forward();
+	std::cout
+		<< "output.size(): " << output2.size() << '\n'
+		<< "output.elemSize(): " << output2.elemSize() << '\n'
+		<< "output.data():\n";
+	for (size_t i = 0; i < output2.cols; ++i)
+		std::cout << output2.at<float>(0, i) << ' ';
+	cout << output2 << endl;
+	cout << "-----" << output2.at<float>(0,0); //<< "-------" << output2.at<double>(1,0) << "------";
 	*/
 
-}
+	Ptr<SelectiveSearchSegmentation> ss = createSelectiveSearchSegmentation();
+	ss->setBaseImage(image);
+	ss->switchToSelectiveSearchFast();
 
-//void training_generation() {}
+	vector<Rect> rects;
+	ss->process(rects);
+	cout << "Total Number of Region Proposals: " << rects.size() << endl;
+
+	while (1) {
+
+		Mat imOut = image.clone();
+		for (int i = 0; i < rects.size(); i++) {
+			if (i < 500){
+				Mat temp = imOut(rects[i]);
+				resize(temp, temp, Size(299, 299), INTER_AREA);
+				Mat blob = blobFromImage(temp, 1.0, Size(299, 299), Scalar(127.5, 127.5, 127.5), true, false);
+				model.setInput(blob);
+				Mat output = model.forward();
+				if(output.at<float>(0, 1) > 0.8)
+					rectangle(imOut, rects[i], Scalar(0, 255, 0));
+			}
+			else
+				break;
+		}
+		imshow("Output", imOut);
+		break;
+	}
+	waitKey(0);
+
+}
 
 double intersection_over_union(vector<int> boxA, vector<int> boxB) {
 
--- source/detecting_boats.h
+++ source/detecting_boats.h
@@ -1,14 +1,17 @@
-#include <opencv2/calib3d.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
+#include <opencv2/dnn/all_layers.hpp>
+#include <opencv2/core.hpp>
+#include "opencv2/ximgproc/segmentation.hpp"
+
+
 #include <fstream>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
+using namespace dnn;
+using namespace cv::ximgproc::segmentation;
 
 //Computing the IOU given two boundary box
 double intersection_over_union(vector<int> boxA, vector<int> boxB);
